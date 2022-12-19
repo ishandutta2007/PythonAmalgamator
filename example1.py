109894b2dd045f0a28b9741df1f75e73a9ed6a48
Ishan <ishandutta2007@gmail.com>
2022-12-19 15:11:18 +0530

    minor

--- example1.py
+++ example1.py
@@ -2,20 +2,39 @@ import sys
 import sympy
 import traceback
 import inspect
+import importlib.util
+import pprint as pp
+from pathlib import Path
 
-
-def run_user_code(envdir):
-    p = list(sympy.primerange(1, 100))
-    source = input(">>> ")
+current_dir =Path.cwd()
+current_file_path=Path(__file__).absolute()
+module_path=importlib.util.find_spec("sympy").origin
+print(module_path, "<= open this path and write before return raise Exception('Deliberate') and run again")
+try:
+    p = list(sympy.primerange(3, 10))#Your sympy function
     print(p)
-    try:
-        exec(source, envdir)
-    except Exception:
-        print("Exception in user code:")
-        print("-"*60)
-        traceback.print_exc(file=sys.stdout)
-        print("-"*60)
+except Exception as e:
+    tb = traceback.format_exc()
+    lines = tb.split("\n")
+    lines = [s.strip() for s in lines]
+    paths = []
+    for l in lines:
+        if "C:\\" in l:
+            pot_paths = l.split(' ')
+            # print("pot_path",pot_paths)
+            for pot_path in pot_paths:
+                if "C:\\" in pot_path:
+                    paths.append(pot_path)
+                    break
+    paths = [s.strip().replace(',','').replace('"','') for s in paths]
+    paths = list(set(paths))
+    pp.pprint(paths)
+    print(current_file_path)
+    if len(paths)==0:
+        paths=['C:\\Python310\\Lib\\site-packages\\sympy\\ntheory\\generate.py']
+    import shutil
+    for path in paths:
+        shutil.copyfile(path, current_dir+path.split("/")[-1])
+        
 
-envdir = {}
-while True:
-    run_user_code(envdir)
+
